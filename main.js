43c4bd88ef3ae8ef29b7f3e00f95feaa8ac9bdff
keenager <keenager84@gmail.com>
2021-05-31 02:54:40 +0900

    시버 생성

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 var http = require('http');
+var url = require('url');
 
 var app = http.createServer(function(request, response){
+    var _url = request.url; // 포트번호 뒷부분('/'부터)
+    var queryData = url.parse(_url, true).query; // '?' 뒷부분
+    var pathname = url.parse(_url, true).pathname;  // '/'부터 '?' 앞까지
 
+    if(pathname === '/'){
+
+        response.writeHead(200);
+        response.end('Success!');
+    }else{
+        response.writeHead(404);
+        response.end();
+    }
 });
 
 app.listen(3000);
