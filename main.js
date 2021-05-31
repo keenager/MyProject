4938f43cb3657155a097ac1fdce2bddb446e5cef
keenager <keenager84@gmail.com>
2021-05-31 18:12:33 +0900

    main.js

--- main.js
+++ main.js
@@ -1,17 +1,29 @@
-var http = require('http');
-var url = require('url');
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const calmod = require('./lib/calmod');
 
-var app = http.createServer(function(request, response){
-    var _url = request.url; // 포트번호 뒷부분('/'부터)
-    var queryData = url.parse(_url, true).query; // '?' 뒷부분
-    var pathname = url.parse(_url, true).pathname;  // '/'부터 '?' 앞까지
+let app = http.createServer(function(request, response){
+    let _url = request.url; // 포트번호 뒷부분('/'부터)
+    let queryData = url.parse(_url, true).query; // '?' 뒷부분
+    let pathname = url.parse(_url, true).pathname;  // '/'부터 '?' 앞까지
 
     if(pathname === '/'){
 
         response.writeHead(200);
         response.end('Success!');
-    } else if(pathname === '/Calendar'){
-
+    } else if(pathname === '/calendar'){
+        calmod.html(request, response, '/index.html');
+    } else if(pathname === '/calendar.css'){
+        calmod.src(request, response, pathname);
+    } else if(pathname === '/calendar.js'){
+        calmod.src(request, response, pathname);
+    } else if(pathname === '/inputWindow.html'){
+        calmod.html(request, response, pathname);
+    } else if(pathname === '/todolist.html'){
+        calmod.html(request, response, pathname);
+    } else if(pathname === '/todolist.js'){
+        calmod.src(request, response, pathname);
     } else{
         response.writeHead(404);
         response.end();
