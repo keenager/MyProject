77652b21904bb4ccb3666cbfd49b6a3d744dbe7c
keenager <keenager84@gmail.com>
2021-08-18 22:30:09 +0900

    bookSchedule 저장,삭제 구현

--- public/bookSchedule.js
+++ public/bookSchedule.js
@@ -1,43 +1,110 @@
-let scdData = {basis: 'fromToday'};
+let scdConfig = {basis: 'fromToday'};
+let scdData = {};
 
-button.onclick = () => {
-    displayScd(+totalPage.value, +goalPage.value);
-}
-
-let inputs = document.querySelectorAll('input[type=radio]');
-for (input of inputs) {
-    input.onchange = event => {
-        scdData.basis = event.currentTarget.value;
-        displayScd(+totalPage.value, +goalPage.value);
+//저장된 스케줄
+let listDiv = document.querySelector('#savedList ul');
+let curSavedData = new Object();
+let selectedDataTitle = '';
+let lists = new Array();
+if( localStorage.getItem('bookSchedule') === null ) {
+    listDiv.parentElement.innerHTML = '저장된 자료가 없습니다.';
+} else {
+    curSavedData = JSON.parse(localStorage.getItem('bookSchedule'));
+    lists = Object.keys(curSavedData);
+    for(let list of lists) {
+        listDiv.insertAdjacentHTML('beforeend', `
+            <li><a href='javascript:void(0)'>${list}</a></li>
+            `
+        );
     }
 }
 
+//이벤트 처리
+let inputs = document.querySelectorAll('input[type=radio]');
+for (input of inputs) {
+    input.onchange = event => {
+        scdConfig.basis = event.currentTarget.value;
+        setScd(+totalPage.value, +goalPage.value);
+        displayScd(scdData);
+        scdConfig.type = 'new';
+        selectedDataTitle = '';
+    }
+}
+
+calBtn.onclick = () => {
+    setScd(+totalPage.value, +goalPage.value);
+    displayScd(scdData);
+    scdConfig.type = 'new';
+    selectedDataTitle = '';
+}
+
+let listElemArr = Array.from(document.querySelectorAll('li > a'));
+for(let listElem of listElemArr) {
+    listElem.addEventListener('click', event => {
+        selectedDataTitle = event.currentTarget.textContent;
+        scdData = curSavedData[selectedDataTitle];
+        displayScd(scdData);
+        scdConfig.type = 'saved';
+    });
+}
+
+saveBtn.onclick = () => {
+    let title = prompt('책 제목을 입력하세요.', scdConfig.type === 'new' ? '' : selectedDataTitle);
+    if(title === null) {
+        return
+    } else if(title === '') {
+        title = '제목 없음';
+    }
+    curSavedData[title] = scdData;
+    localStorage.setItem('bookSchedule', JSON.stringify(curSavedData, null, 2));
+    location.reload();
+}
+
+delBtn.onclick = () => {
+    if(scdConfig.type === 'new') {
+        return
+    }
+    delete curSavedData[selectedDataTitle];
+    localStorage.setItem('bookSchedule', JSON.stringify(curSavedData, null, 2));
+    location.reload();
+}
+
+//함수
 function calcScd(totalPage, goalpage) {
-    scdData.day = Math.ceil(totalPage / goalpage);
-    scdData.last = totalPage % goalpage;
+    scdConfig.day = Math.ceil(totalPage / goalpage);
+    scdConfig.last = totalPage % goalpage;
 }
 
 function getYMD(idx) {
-    let date = scdData.basis === 'fromToday' ? new Date() : new Date(specDate.value);
+    let date = scdConfig.basis === 'fromToday' ? new Date() : new Date(specDate.value);
     date.setDate(date.getDate() + idx);
     return date
 }
 
-function displayScd(totalPage, goalpage) {
-    let tbody = document.querySelector('tbody');
-    tbody.innerHTML = '';
+function setScd(totalPage, goalpage) {
     calcScd(totalPage, goalpage);
+    scdData = {};
     let startPage = 1;
     let endPage;
-    for (let i = 0; i < scdData.day; i++) {
-        endPage = i === scdData.day - 1 ?
-                startPage + scdData.last - 1 :
+    let ymdStr, pageStr;
+    for (let i = 0; i < scdConfig.day; i++) {
+        endPage = i === scdConfig.day - 1 ?
+                startPage + scdConfig.last - 1 :
                 startPage + goalpage - 1;
-        let newRow = tbody.insertRow(-1);
-        newRow.insertCell(0).innerHTML = `
-            ${getYMD(i).getFullYear()}년 ${getYMD(i).getMonth() + 1}월 ${getYMD(i).getDate()}일:`;
-        newRow.insertCell(1).innerHTML = `
-            ${startPage}쪽 ~ ${endPage}쪽`;
+        ymdStr = `${getYMD(i).getFullYear()}년 ${getYMD(i).getMonth() + 1}월 ${getYMD(i).getDate()}일:`;
+        pageStr =  `${startPage}쪽 ~ ${endPage}쪽`;
+        scdData[ymdStr] = pageStr;
         startPage = endPage + 1;
+    }
+}
+
+function displayScd(data) {
+    let tbody = document.querySelector('tbody');
+    tbody.innerHTML = '';
+    for(let key in data) {
+        let newRow = tbody.insertRow(-1);
+        newRow.insertCell(0).innerHTML = key;
+        newRow.insertCell(1).innerHTML = data[key];        
+        newRow.insertCell(2).innerHTML = "<input type='number'>";
     }
 }
