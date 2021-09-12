23c7ca4c7a54470a1bb0af2f15b5926494b2067f
keenager <keenager84@gmail.com>
2021-09-13 00:27:46 +0900

    diet 부분 수정(오늘 기준 최근 7일, 1달)

--- public/diet.js
+++ public/diet.js
@@ -1,31 +1,56 @@
-let w_config = new Config('line', ['Mon', 'Tues', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun']);
-let w_date = new Date();
-w_date.setDate( 
-    w_date.getDate() 
-    - ( w_date.getDay() == 0 ? 6 : w_date.getDay() - 1 ) 
-);   // 이번 주 월요일의 날짜를 구해서 첫 날로 지정.
+const curDate = new Date();
 
-let temp = new Date(w_date);
-temp.setDate(w_date.getDate() + 6);
-document.getElementById('thisWeek').textContent = `
-    ${w_date.getMonth() + 1}월 ${w_date.getDate()}일 ~ ${temp.getMonth() + 1}월 ${temp.getDate()}일
+//오늘 포함 최근 1주일의 요일을 배열로
+const dayTemplateArr = ['일', '월', '화', '수', '목', '금', '토'];
+let sevenDaysFromTodayArr = [];
+for(let i = 0; i < 7; i++) {
+    let temp = curDate.getDay() - i;
+    temp = temp >= 0 ? temp : 7 - i;
+    sevenDaysFromTodayArr[i] = dayTemplateArr[temp];
+}
+
+//config와 첫째날 설정
+const w_config = new Config('line', sevenDaysFromTodayArr.reverse());
+let w_date = new Date();
+w_date.setDate(curDate.getDate() - 6);
+
+//제목 표시
+document.getElementById('week').textContent = `
+    ${w_date.getMonth() + 1}월 ${w_date.getDate()}일 ~ ${curDate.getMonth() + 1}월 ${curDate.getDate()}일
 `;
+
+//화면 표시
 displayChart( 'weeklyChart', w_config, w_date );
 
-let m_date = new Date();
-document.getElementById('thisMonth').textContent = m_date.getMonth() + 1 + '월';
-function dateArr() {
-    let arr = [];
-    let n = 32 - new Date( m_date.getFullYear(), m_date.getMonth(), 32 ).getDate();   // 이번 달이 몇일까지 있는지 구하기. 또는 lastDate[date.getMonth()]
-    for (let i = 0; i < n; i++) {
-        arr[i] = i + 1;
-    }
-    return arr;
+//-------------------
+
+//오늘 포함 최근 1달의 날짜를 배열로
+let thirtyDaysFromTodayArr = [];
+
+const lastDate = beforeMonthLastDate(new Date());
+
+for(let i = 0; i < 31; i++) {
+    let temp = curDate.getDate() - i;
+    temp = temp > 0 ? temp : lastDate + temp;
+    thirtyDaysFromTodayArr[i] = temp;
 }
-let m_config = new Config( 'line', dateArr() );
-m_date.setDate(1);
+
+//config와 첫째날 설정
+const m_config = new Config( 'line', thirtyDaysFromTodayArr.reverse() );
+let m_date = new Date();
+m_date.setDate(m_date.getDate() - 30);
+
+//제목 표시
+let m_title = curDate.getMonth() + 1 + '월';
+if(m_date.getMonth() !== curDate.getMonth()) {
+    m_title = m_date.getMonth() + 1 + '~' + m_title;
+}
+document.getElementById('month').textContent = m_title;
+
+//화면 표시
 displayChart('monthlyChart', m_config, m_date);
 
+//---------------
 
 function Config(type, labels) {
     this.type = type;
@@ -42,15 +67,23 @@ function Config(type, labels) {
         scales: {
             y: {
                 min: 50,
-                max: 70,
+                max: 60,
             }
         }
     }
 }
 
+function beforeMonthLastDate(dateObj) {
+    dateObj.setDate(1);
+    dateObj.setDate(dateObj.getDate() - 1);
+    return dateObj.getDate()
+}
+
 function displayChart(elemId, config, date) {
     for(let i = 0; i < config.data.labels.length; i++) {
-        dateId = date.getFullYear() + '-' + modifyMonth(date.getMonth()) + '-' + modifyDate(date.getDate() + i);
+        let tempDate = new Date(date);
+        tempDate.setDate(tempDate.getDate() + i);
+        dateId = tempDate.getFullYear() + '-' + modifyMonth(tempDate.getMonth()) + '-' + modifyDate(tempDate.getDate());
         // dateId 를 이용해서 서버에서 weight 데이터 불러온 뒤,
         // w_config.data.datasets[0].data[i] 에 입력
         fetch('/diet/db_read/' + dateId)
@@ -58,10 +91,10 @@ function displayChart(elemId, config, date) {
             if(response.status === 200) return response.json()
             else console.log(response.statusText);
         })
-        .then(data => {
-            if(data){
-                for(e of data){
-                    config.data.datasets[0].data[i] = e.weight;
+        .then(result => {
+            if(result){
+                for(e of result){
+                    config.data.datasets[0].data[i] = result[0].weight;
                 }
                 myChart.update();
             }
