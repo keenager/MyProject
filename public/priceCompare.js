52069a1934dbb8be0e215ce8d9657a5964b4e46b
keenager <keenager84@gmail.com>
2021-09-30 00:06:48 +0900

    가격 비교 보완(검색 결과 필터링, 검색 목록 구현)

--- public/priceCompare.js
+++ public/priceCompare.js
@@ -1,4 +1,38 @@
 const marts = ['ssg', 'hp'];
+let savedQuery;
+
+//저장된 스케줄
+let listDiv = document.querySelector('.list_container');
+if( localStorage.getItem('priceCompare') === '[]' || localStorage.getItem('priceCompare') === null ) {
+    listDiv.innerHTML = '저장된 목록이 없습니다.';
+    savedQuery = [];
+} else {
+    savedQuery = JSON.parse(localStorage.getItem('priceCompare'));
+    for(let item of savedQuery) {
+        listDiv.insertAdjacentHTML('beforeend', `
+            <div class='list'><a href='javascript:void(0)'>${item}</a><button class='delBtn'>X</button></div>
+            `
+        );
+    }
+
+    //검색 목록 클릭 이벤트
+    let listElemArr = Array.from(document.querySelectorAll('li > a'));
+    for(let listElem of listElemArr) {
+        listElem.addEventListener('click', event => {
+            document.querySelector('input').value = event.target.textContent;
+        });
+    }
+    //검색 목록 삭제
+    let xElemArr = Array.from(document.querySelectorAll('.delBtn'));
+    for(let xElem of xElemArr) {
+        xElem.addEventListener('click', event => {
+            let index = savedQuery.findIndex(item => item === event.target.previousSibling.textContent);
+            savedQuery.splice(index, 1);
+            localStorage.setItem('priceCompare', JSON.stringify(savedQuery));
+            window.location.reload();
+        });
+    }
+}
 
 function enterkey() {
     if(window.event.keyCode === 13) {
@@ -7,13 +41,18 @@ function enterkey() {
 }
 
 document.querySelector('button').addEventListener('click', () => {
-    marts.forEach(mart => document.querySelector('#' + mart).innerHTML = '');
     let query = document.querySelector('input').value;
+    if(!savedQuery.includes(query)) {
+        savedQuery.push(query);
+    }
+    localStorage.setItem('priceCompare', JSON.stringify(savedQuery));
+
+    marts.forEach(mart => document.querySelector('#' + mart).innerHTML = '');
     let requests = marts.map(mart => fetch(`/priceCompare/get_prices/${mart}/${query}`));
     Promise.all(requests)
     .then(responses => Promise.all(responses.map(r => r.json())))
-    .then(lists => lists.forEach( (list, index) => {
-        let elem = document.getElementById(marts[index]);
+    .then(lists => lists.forEach( (list, index) => {        // lists: 마트별 검색결과 리스트
+        let elem = document.getElementById(marts[index]);   // list: 특정 마트 검색결과 리스트
         for(let item of list) {
             elem.insertAdjacentHTML('beforeend', `
                 <div class='item'>
